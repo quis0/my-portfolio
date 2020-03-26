d63b890477f506b6ff373c3766bb2daac17d2493
quis0 <zotkevich@bk.ru>
2020-03-26 22:52:13 +0700

    7!

--- sprint-5/07-anagram/script.js
+++ sprint-5/07-anagram/script.js
@@ -5,14 +5,25 @@
  * Напишите функцию, проверяющую, являются ли две строки анаграммами друг друга
  * (регистр букв не имеет значения). Для простоты примите, что в этих строках
  * нет пробелов и знаков препинания.
- * 
+ *
 */
 
 function anagram(str1, str2) {
-    // Напишите код здесь
+  if (str1.length !== str2.length)
+    return false;
+  let newStr1 = str1.toLowerCase();
+  let newStr2 = str2.toLowerCase();
+  if (newStr1 === newStr2) {
+    return false;
+  }
+  let arr1 = newStr1.split('').sort();
+  let arr2 = newStr2.split('').sort();
+  newStr1 = arr1.join('');
+  newStr2 = arr2.join('');
+  return (newStr1 === newStr2);
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(anagram('finder', 'Friend')); // true
-console.log(anagram('hello', 'bye')); // false
+console.log(anagram('up', 'UP')); // true
+// console.log(anagram('hello', 'bye')); // false
