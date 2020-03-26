30824060821f11c28824109fbc5ee21f7fd00a1f
quis0 <zotkevich@bk.ru>
2020-03-26 21:39:56 +0700

    1 task is ready.

--- sprint-5/01-palindrome/script.js
+++ sprint-5/01-palindrome/script.js
@@ -7,26 +7,28 @@
  *
  * Напишите функцию palindrome(str), принимающую как аргумент строку.
  * Функция должна вернуть true, если строка — палиндром, и false, если нет.
- * 
+ *
  * Считайте, что на вход всегда передаётся слово: то есть знаков препинания
  * и пробелов в аргументе быть не может.
- * 
+ *
 */
 
 function palindrome(str) {
-    // Напишите код здесь
+  let newStr = str.toLowerCase().replace(/[^A-Za-zА-Яа-яЁё]/g, "");
+  let reversedStr = newStr.split('').reverse().join('');
+  return(newStr===reversedStr);
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(palindrome('топот')); // должно быть true
+console.log(palindrome('топ!от')); // должно быть true
 console.log(palindrome('Saippuakivikauppias')); // true
 console.log(palindrome('привет')); // false
 
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
- * 
+ *
  * palindrome('О, лета тело!'); // true
- * 
+ *
 */
