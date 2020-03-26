b07bbddea32aba502c4f453100499d7a0f73caf8
quis0 <zotkevich@bk.ru>
2020-03-27 00:16:23 +0700

    8/14

--- sprint-5/08-fibonacci/script.js
+++ sprint-5/08-fibonacci/script.js
@@ -3,15 +3,50 @@
  *
  * Последовательность Фибоначчи — это порядок чисел, где каждое последующее
  * число является суммой двух предыдущих: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
- * 
+ *
  * Напишите функцию, которая принимает на вход число n и возвращает n-й элемент
  * последовательности Фибоначчи.
 */
 
 function fibonacci(n) {
-    // Напишите код здесь
+  if (typeof (n) !== 'number' || (n ^ 0) !== n) {
+    console.error('Wrong input!');
+    return undefined;
+  }
+  if (!fibonacci.cache) {
+    fibonacci.cache = {};
+  }
+  if (n in fibonacci.cache) {
+    return fibonacci.cache[n];
+  }
+
+  let firstNumber = 0, secondNumber = 1, sum = 0;
+
+  if (n < 0) {
+    for (let i = -1; i > n; i--) {
+      sum = firstNumber - secondNumber;
+      firstNumber = secondNumber;
+      secondNumber = sum;
+    }
+    fibonacci.cache[n] = secondNumber;
+  }
+  else if (n === 0) {
+    fibonacci.cache[n] = 0;
+  }
+  else {
+    for (let i = 1; i < n; i++) {
+      sum = firstNumber + secondNumber;
+      firstNumber = secondNumber;
+      secondNumber = sum;
+    }
+    fibonacci.cache[n] = secondNumber;
+  }
+
+  return fibonacci.cache[n];
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fibonacci(4)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
+for (let i = -5; i <= 5; i++) {
+  console.log(fibonacci(i));
+}
