d0bf0ff552f83691e7d8df311e7628c6e6a0935c
quis0 <zotkevich@bk.ru>
2020-03-28 04:07:11 +0700

    13/14

--- sprint-5/13-sum-of-two/script.js
+++ sprint-5/13-sum-of-two/script.js
@@ -5,11 +5,46 @@
  * и целое число num. Функция должна вернуть true, если в переданном массиве
  * есть какие-то два числа, чья сумма равна num. Если же такой пары чисел нет,
  * функция должна вернуть false.
- * 
+ *
 */
 
+function isIntegerArray(arr) {
+  let isIntegerArray = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number' || (!Number.isInteger(arr[i]))) {
+      isIntegerArray = false;
+      break;
+    }
+  }
+  return isIntegerArray;
+}
+
 function sumOfTwo(arr, sum) {
-    // Напишите код здесь
+
+  if(!isIntegerArray(arr)) {
+    console.error('Wrong input');
+    return undefined;
+  }
+
+  if (typeof sum !== 'number' || !Number.isInteger(sum)) {
+    console.error('Wrong input');
+    return undefined;
+  }
+
+  let newArr = arr.map(function (item) {
+    return sum - item;
+  });
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === newArr[j]) {
+        return true;
+      }
+    }
+  }
+
+
+  return false;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
