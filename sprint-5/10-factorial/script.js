247b814324b9392aa139200fbf049b939597ae92
quis0 <zotkevich@bk.ru>
2020-03-27 11:47:48 +0700

    10/14

--- sprint-5/10-factorial/script.js
+++ sprint-5/10-factorial/script.js
@@ -4,15 +4,23 @@
  * Напишите функцию factorial(n), возвращающую факториал неотрицательного
  * целого числа. Факториал — это произведение всех натуральных чисел
  * от 1 до n включительно. Факториал 0 равен 1.
- * 
+ *
 */
 
 function factorial(n) {
-    // Напишите код здесь
+  if (typeof n !== 'number' || n < 0 || !Number.isInteger(n)) {
+    console.error('Wrong input');
+    return undefined;
+  }
+  let factorial = 1;
+  for (let i = 1; i < n; i++) {
+    factorial *= i + 1;
+  }
+  return factorial;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(factorial(0)); // 1
 console.log(factorial(1)); // 1
-console.log(factorial(6)); // 720
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 720
