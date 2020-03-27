796a95400b63b1afe1dc2cf9988b24abbc0df3b8
quis0 <zotkevich@bk.ru>
2020-03-27 22:58:34 +0700

    11/14

--- sprint-5/11-intersection/script.js
+++ sprint-5/11-intersection/script.js
@@ -4,14 +4,52 @@
  * Напишите функцию intersection(arr1, arr2). Она должна принимать
  * на вход два массива целых чисел. Функция должна вернуть новый
  * массив чисел, содержащихся в обоих исходных массивах.
- * 
+ *
 */
 
+function uniq(arr) {
+  if (!Array.isArray(arr)) {
+    console.error("It's not an array");
+    return undefined;
+  }
+  let newArr = [];
+  arr.forEach(function (number) {
+    if (!newArr.includes(number)) {
+      newArr.push(number);
+    }
+  });
+  return newArr;
+}
+
+function isIntegerArray(arr) {
+  let isIntegerArray = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number' || (!Number.isInteger(arr[i]))) {
+      isIntegerArray = false;
+      break;
+    }
+  }
+  return isIntegerArray;
+}
+
+
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+  if (isIntegerArray(arr1) && isIntegerArray(arr2)) {
+    let newArr1 = uniq(arr1);
+    let newArr2 = uniq(arr2);
+    let res = [];
+    newArr1.forEach(function (number) {
+      if (newArr2.includes(number)) {
+        res.push(number);
+      }
+    });
+    return res;
+  }
+  console.error('Wrong input!');
+  return undefined;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(intersection([1, 5, 4, 2], [8, 91, 4, 1, 3])); // [4, 1]
-console.log(intersection([1, 5, 4, 2], [7, 12])); // []
+console.log(intersection([1, 5, 4, 2, 4.4], [8, 91, 4, 1, 4])); // [4, 1]
+
