ced5fdaedf641a1ea7ea4880954167c04d46f6c5
quis0 <zotkevich@bk.ru>
2020-03-28 03:48:19 +0700

    12/14

--- sprint-5/12-count-zeros/script.js
+++ sprint-5/12-count-zeros/script.js
@@ -3,14 +3,33 @@
  *
  * Напишите функцию countZeros(n), принимающую на вход целое неотрицательное
  * число n. Возвращать функция должна количество нулей, содержащихся в аргументе.
- * 
+ *
 */
 
 function countZeros(n) {
-    // Напишите код здесь
+
+  if (typeof n !== 'number' || !Number.isInteger(n) || n < 0) {
+    console.error('Wrong input!');
+    return undefined;
+  }
+
+
+  let string = '';
+  for (let i = 1; i <= n; i++) {
+    string = string.concat(i.toString());
+  }
+
+  let totalZeros = 0;
+  while (string.search('0') !== -1) {
+    string = string.substring(string.search('0') + 1);
+
+    totalZeros++;
+  }
+
+  return totalZeros;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
-console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+console.log(countZeros(1)); // 2 – два нуля, по одному в числах 10 и 20
+console.log(countZeros(20)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
