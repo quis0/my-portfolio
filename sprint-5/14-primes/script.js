a050bd29ce52fc21fd277220ff795c0541732a00
quis0 <zotkevich@bk.ru>
2020-03-28 04:12:46 +0700

    14/14 =)

--- sprint-5/14-primes/script.js
+++ sprint-5/14-primes/script.js
@@ -3,11 +3,48 @@
  *
  * Напишите функцию primes(n). Её единственный аргумент — целое число n.
  * Функция должна возвращать массив простых чисел от 2 до n.
- * 
+ *
 */
 
+function isPrime(n) {
+  if (typeof n === 'number' && isFinite(n) && Number.isInteger(n)) {
+    if (!isPrime.cache) {
+      isPrime.cache = {};
+    }
+    if (n in isPrime.cache) {
+      return isPrime.cache[n];
+    }
+    if (n === 0 || n === 1) {
+      isPrime.cache[n] = false;
+      return isPrime.cache[n];
+    }
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        isPrime.cache[n] = false;
+        return isPrime.cache[n];
+      }
+    }
+    isPrime.cache[n] = true;
+    return isPrime.cache[n];
+  }
+  console.error('Wrong input!');
+  return undefined;
+}
+
 function primes(num) {
-    // Напишите код здесь
+
+  if (typeof num !== 'number' || !Number.isInteger(num)) {
+    console.error('Wrong input');
+    return undefined;
+  }
+
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    if (isPrime(i)) {
+      arr.push(i);
+    }
+  }
+  return arr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
